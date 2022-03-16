534692c0ef077027363ae861a12e5b774b5e5b6c
andresouza9 <56726314+andresouza9@users.noreply.github.com>
2022-03-15 21:10:27 -0300

    .

--- js/main.js
+++ js/main.js
@@ -202,3 +202,10 @@ window.onload = function() {
   document.body.appendChild(css);
 };
 
+function showStuff(id, text, btn) {
+    document.getElementById(id).style.display = 'block';
+    // hide the lorem ipsum text
+    document.getElementById(text).style.display = 'none';
+    // hide the link
+    btn.style.display = 'none';
+}
